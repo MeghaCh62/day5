0a9d5c93c7dd4a5f971ac93027165e114ad5a560
Megha <megha.maheswari@terralogic.com>
2020-12-24 16:52:06 +0530

    day5

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import {BrowserRouter as Router, HashRouter, Switch , Route} from "react-router-dom";
+import tables from "./component/tables";
+
+function App(){
+    return (
+      <Router>
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact component={tables}/>
+          {/* <Route component={Error} /> */}
+          </Switch>
+      </HashRouter>
+    </Router>
+    );
+  }
+
+
+export default App;
